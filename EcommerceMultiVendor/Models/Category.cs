f73975bacf4dd9d2dd7fa1f17ed6d87a5d742bc8
rajanlama786 <rajan.lama786@gmail.com>
2022-11-01 23:05:04 +0545

    Add: Category class

---
+++ EcommerceMultiVendor/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿namespace EcommerceMultiVendor.Models
+{
+    public class Category
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+
+        public int GetID() => Id;
+
+        public string GetCatName() => Name;
+
+        public string GetDescription() => Description;
+
+        public void SetId( int id ) {
+            Id = id;
+        }
+
+        public void SetCatName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetDescritpion( string description )
+        {
+            Description = description;
+        }
+    }
+}
